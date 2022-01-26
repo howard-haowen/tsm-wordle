e4f6b37eb0a3df0a62b41b6bd50e2fb14db88775
roedoejet <aidanpine@shaw.ca>
2022-01-26 14:30:29 -0800

    docs: add advanced user warning in config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,11 +2,14 @@ export const CONFIG = {
     tries: 6, // This changes how many tries you get to finish the wordle
     language: "YourLanguageHere", // This changes the display name for your language
     wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-    googleAnalytics: "", // You can use this if you use Google Analytics
     author: "YourNameHere", // Put your name here so people know who made this Wordle!
     authorWebsite: "YourLinkHere", // Put a link to your website or social media here
     wordListSource: "YourDictionarySource", // Describe the source material for your words here
     wordListSourceLink: "YourDictionaryLink", // Put a link to the source material for your words here
+    //
+    // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
+    //
+    googleAnalytics: "", // You can use this if you use Google Analytics
     shuffle: false, // whether to shuffle the words in the wordlist
     normalization: 'NFC' // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
 }
