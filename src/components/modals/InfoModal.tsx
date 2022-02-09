7116add05d9610dd34bd1767bd5ad2aad52604c9
roedoejet <aidanpine@shaw.ca>
2022-02-09 12:28:46 -0800

    style: prettify

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -11,8 +11,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
-        Guess the WORDLE in {CONFIG.tries} tries. After each guess, the color of the tiles
-        will change to show how close your guess was to the word.
+        Guess the WORDLE in {CONFIG.tries} tries. After each guess, the color of
+        the tiles will change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -12,5 +12,5 @@ export const CONFIG = {
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: 'January 1, 2022 00:00:00' // what date and time to start your game from
+  startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
 }
