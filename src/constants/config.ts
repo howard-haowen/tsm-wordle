71bdadee72911315f626140e214d1966f498ac06
Haowen Jiang <howard.haowen@gmail.com>
2022-03-13 00:54:16 +0800

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,18 +1,18 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
-  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  language: 'Taiwanese Southern Min', // This changes the display name for your language
+  wordLength: 4, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  author: 'Haowen Jiang', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'https://howard-haowen.github.io/', // Put a link to your website or social media here
+  wordListSource: '臺灣閩南語常用詞辭典', // Describe the source material for your words here
+  wordListSourceLink: 'https://github.com/g0v/moedict-data-twblg', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'en', // the default interface language
-  availableLangs: ['en', 'es'], // the options available to the user for translation languages
+  startDate: 'March 1, 2022 00:00:00', // what date and time to start your game from
+  defaultLang: 'zh', // the default interface language
+  availableLangs: ['zh', 'en'], // the options available to the user for translation languages
 }
