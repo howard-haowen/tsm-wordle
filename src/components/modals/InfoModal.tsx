12e6dd4c1ca1527c9ab43f66f29c4a106d1b2a97
Haowen Jiang <howard.haowen@gmail.com>
2022-03-16 21:29:03 +0800

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -2,14 +2,29 @@ import { Cell } from '../grid/Cell'
 import { BaseModal } from './BaseModal'
 import { CONFIG } from '../../constants/config'
 import { useTranslation } from 'react-i18next'
+import 'i18next'
 
 type Props = {
   isOpen: boolean
   handleClose: () => void
 }
 
+interface Letter {
+  letter: string
+  highlight: boolean
+}
+
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   const { t } = useTranslation()
+  const firstExampleWord: Letter[] = t('firstExampleWord', {
+    returnObjects: true,
+  })
+  const secondExampleWord: Letter[] = t('secondExampleWord', {
+    returnObjects: true,
+  })
+  const thirdExampleWord: Letter[] = t('thirdExampleWord', {
+    returnObjects: true,
+  })
   return (
     <BaseModal title={t('howToPlay')} isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
@@ -17,29 +32,37 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value={t('firstExampleWord.0')} status="correct" />
-        <Cell value={t('firstExampleWord.1')} />
-        <Cell value={t('firstExampleWord.2')} />
-        <Cell value={t('firstExampleWord.3')} />
-        <Cell value={t('firstExampleWord.4')} />
+        {Array.isArray(firstExampleWord) &&
+          firstExampleWord.map((el: Letter) => {
+            if (el.highlight) {
+              return <Cell key={el.letter} value={el.letter} status="correct" />
+            } else {
+              return <Cell key={el.letter} value={el.letter} />
+            }
+          })}
       </div>
       <p className="text-sm text-gray-500">{t('correctSpotInstructions')}</p>
-
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value={t('secondExampleWord.0')} />
-        <Cell value={t('secondExampleWord.1')} />
-        <Cell value={t('secondExampleWord.2')} status="present" />
-        <Cell value={t('secondExampleWord.3')} />
-        <Cell value={t('secondExampleWord.4')} />
+        {Array.isArray(secondExampleWord) &&
+          secondExampleWord.map((el) => {
+            if (el.highlight) {
+              return <Cell key={el.letter} value={el.letter} status="present" />
+            } else {
+              return <Cell key={el.letter} value={el.letter} />
+            }
+          })}
       </div>
       <p className="text-sm text-gray-500">{t('wrongSpotInstructions')}</p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value={t('thirdExampleWord.0')} />
-        <Cell value={t('thirdExampleWord.1')} />
-        <Cell value={t('thirdExampleWord.2')} />
-        <Cell value={t('thirdExampleWord.3')} status="absent" />
-        <Cell value={t('thirdExampleWord.4')} />
+        {Array.isArray(thirdExampleWord) &&
+          thirdExampleWord.map((el) => {
+            if (el.highlight) {
+              return <Cell key={el.letter} value={el.letter} status="absent" />
+            } else {
+              return <Cell key={el.letter} value={el.letter} />
+            }
+          })}
       </div>
       <p className="text-sm text-gray-500">{t('notInWordInstructions')}</p>
     </BaseModal>
